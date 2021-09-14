475ef96e9d0a869203f71ea24cb221ac074ce3a6
JOSDEAD <jmmurillo61@gmail.com>
2021-09-13 18:02:04 -0600

    adding digital clock
    adding digital clock

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 window.addEventListener("load", function () {
     const secondsHand = this.document.querySelector('.second-hand');
     const minutesHand = this.document.querySelector('.min-hand');
-    const hourHand = this.document.querySelector('.hour-hand');
-    setInterval(()=>setTime(secondsHand,minutesHand,hourHand), 1000);
-    console.log(hourHand);
+    const hourHand = this.document.querySelector('.hour-hand');   
+    const hours = this.document.querySelector('.hours');
+        const minutes = this.document.querySelector('.minutes');
+
+        const seconds = this.document.querySelector('.seconds');
+
+     setInterval(()=>setTime(secondsHand,minutesHand,hourHand,hours,minutes,seconds), 1000);
+    console.log(hours);
 }, false);
 
-const setTime = (secondsHand,minutesHand,hourHand) => {
+const setTime = (secondsHand,minutesHand,hourHand,hours,minutes,seconds) => {
     const now = new Date();
     const secondsDegrees =  ((now.getSeconds() / 60 ) * 360 ) +90;
     const minutesDegrees = ((now.getMinutes() / 60 ) * 360 ) +90;
@@ -15,4 +20,8 @@ const setTime = (secondsHand,minutesHand,hourHand) => {
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    hours.textContent = `${now.getHours()}:`;    
+    minutes.textContent = `${now.getMinutes() < 10 ? '0' : ''}${now.getMinutes()}:`;
+    seconds.textContent = `${now.getSeconds() < 10 ? '0' : ''}${now.getSeconds()}`;
+
 }
