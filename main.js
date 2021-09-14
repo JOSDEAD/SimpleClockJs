cd2dc24621927b7a1436c596d8e4dbd4104ff96c
JOSDEAD <jmmurillo61@gmail.com>
2021-09-13 18:25:08 -0600

    Refractoring code
    Refractoring code

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
-window.addEventListener("load", function () {
-    const secondsHand = this.document.querySelector('.second-hand');
-    const minutesHand = this.document.querySelector('.min-hand');
-    const hourHand = this.document.querySelector('.hour-hand');   
-    const hours = this.document.querySelector('.hours');
-        const minutes = this.document.querySelector('.minutes');
+window.addEventListener(
+  "load",
+  function () {
+    const secondsHand = this.document.querySelector(".second-hand");
+    const minutesHand = this.document.querySelector(".min-hand");
+    const hourHand = this.document.querySelector(".hour-hand");
+    const hours = this.document.querySelector(".hours");
+    const minutes = this.document.querySelector(".minutes");
 
-        const seconds = this.document.querySelector('.seconds');
+    const seconds = this.document.querySelector(".seconds");
 
-     setInterval(()=>setTime(secondsHand,minutesHand,hourHand,hours,minutes,seconds), 1000);
-    console.log(hours);
-}, false);
-
-const setTime = (secondsHand,minutesHand,hourHand,hours,minutes,seconds) => {
-    const now = new Date();
-    const secondsDegrees =  ((now.getSeconds() / 60 ) * 360 ) +90;
-    const minutesDegrees = ((now.getMinutes() / 60 ) * 360 ) +90;
-    const hourDegrees = ((now.getHours() / 12 ) * 360 ) +90;
-    console.log(hourDegrees);
-    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    hours.textContent = `${now.getHours()}:`;    
-    minutes.textContent = `${now.getMinutes() < 10 ? '0' : ''}${now.getMinutes()}:`;
-    seconds.textContent = `${now.getSeconds() < 10 ? '0' : ''}${now.getSeconds()}`;
-
-}
+    setInterval(
+      () =>
+        setTime(secondsHand, minutesHand, hourHand, hours, minutes, seconds),
+      1000
+    );
+  },
+  false
+);
+const getDegres = (current,timeTotal) => (current / timeTotal) * 360 + 90;
+const addZero = (currentTime) => `${currentTime < 10 ? "0" : ""}${currentTime}`
+const setTime = (
+  secondsHand,
+  minutesHand,
+  hourHand,
+  hours,
+  minutes,
+  seconds
+) => {
+  const now = new Date();
+  const currentSeconds = now.getSeconds();
+  const currentMinutes = now.getMinutes(); 
+  const currentHours = now.getHours();
+  secondsHand.style.transform = `rotate(${getDegres(currentSeconds,60)}deg)`;
+  minutesHand.style.transform = `rotate(${getDegres(currentMinutes,60)}deg)`;
+  hourHand.style.transform = `rotate(${getDegres(currentHours,12)}deg)`;
+  hours.textContent = `${currentHours}:`;
+  minutes.textContent = `${addZero(currentMinutes)}:`;
+  seconds.textContent = `${addZero(currentSeconds)}`;
+};
